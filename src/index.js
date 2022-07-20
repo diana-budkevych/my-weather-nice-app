c6ba83f23991733293406fc5551df0d028650028
diana-budkevych <morituria@ukr.net>
2022-07-20 20:37:26 +0300

    Added HTML, CSS and JS files

---
+++ src/index.js
@@ -0,0 +1,79 @@
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let today = document.querySelector("#currentDate");
+today.innerHTML = `${day}, ${hours}:${minutes}`;
+
+function search(event) {
+  event.preventDefault();
+  let showCity = document.querySelector("#cityInput");
+  let cityName = document.querySelector("#city");
+  cityName.innerHTML = showCity.value;
+  searchCity(showCity.value);
+}
+let searchFor = document.querySelector("#cityForm");
+searchFor.addEventListener("submit", search);
+
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+
+function searchCity(city) {
+  let apiKey = "3242feb725541ec0d1f1c44544b6ce8e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+function showCurrentTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+}
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "3242feb725541ec0d1f1c44544b6ce8e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showCurrentTemperature);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let showCurrentPosition = document.querySelector("#current-position");
+showCurrentPosition.addEventListener("click", getCurrentLocation);
+//function tempFahrenheit(event) {
+//event.preventDefault();
+//let showFahrenheit = document.querySelector("#current-temp");
+//showFahrenheit.innerHTML = 66;
+//}
+//let changeFahrenheit = document.querySelector("#fahr-temp");
+//changeFahrenheit.addEventListener("click", tempFahrenheit);
+
+//function tempCelsius(event) {
+//event.preventDefault();
+//let showCelsius = document.querySelector("#current-temp");
+//showCelsius.innerHTML = 18;
+//}
+//let changeCelsius = document.querySelector("#cels-temp");
+//changeCelsius.addEventListener("click", tempCelsius);
