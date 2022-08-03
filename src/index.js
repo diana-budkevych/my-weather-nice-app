750b399b462242c95304716c414e1f9cde2b8ad3
diana-budkevych <morituria@ukr.net>
2022-08-03 19:26:21 +0300

    Added the forecast API

--- src/index.js
+++ src/index.js
@@ -20,7 +20,8 @@ if (minutes < 10) {
 let today = document.querySelector("#currentDate");
 today.innerHTML = `${day}, ${hours}:${minutes}`;
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Thu", "Fri", "Sat"];
@@ -41,16 +42,11 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-
-function search(event) {
-  event.preventDefault();
-  let showCity = document.querySelector("#cityInput");
-  let cityName = document.querySelector("#city");
-  cityName.innerHTML = showCity.value;
-  searchCity(showCity.value);
+function getForecast(coordinates) {
+  let apiKey = "3242feb725541ec0d1f1c44544b6ce8e";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
-let searchFor = document.querySelector("#cityForm");
-searchFor.addEventListener("submit", search);
 
 function showTemperature(response) {
   let temperatureElement = document.querySelector("#current-temp");
@@ -64,7 +60,17 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
+function search(event) {
+  event.preventDefault();
+  let showCity = document.querySelector("#cityInput");
+  let cityName = document.querySelector("#city");
+  cityName.innerHTML = showCity.value;
+  searchCity(showCity.value);
+}
+let searchFor = document.querySelector("#cityForm");
+searchFor.addEventListener("submit", search);
 
 function searchCity(city) {
   let apiKey = "3242feb725541ec0d1f1c44544b6ce8e";
@@ -102,5 +108,3 @@ function getCurrentLocation(event) {
 
 let showCurrentPosition = document.querySelector("#current-position");
 showCurrentPosition.addEventListener("click", getCurrentLocation);
-
-displayForecast();
